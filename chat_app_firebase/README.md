559e3a38c66161851db994a5557420f20ca65846
Rahul P <mrsvaz@icloud.com>
2023-07-22 11:53:11 +0530

    all

---
+++ chat_app_firebase/README.md
@@ -0,0 +1,8 @@
+1
+flutterfire configure
+select project
+2
+3
+flutter pub add firebase_core
+flutter pub add firebase_auth
+flutterfire configure
---
+++ chat_app_firebase/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:chat_app_firebase/screens/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'chatApp',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        ),
+
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
